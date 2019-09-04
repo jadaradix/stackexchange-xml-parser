e99c003d125e8bbfc17cd50aacf4f88bcc936aea
James Garner <j@jada.io>
2019-09-04 11:01:48 +0100

    dynamic columns

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const {strict: assert} = require('assert')
 const handleLines = require('./src/handleLines/handleLines')
 
-const [,,input, output] = process.argv
+let [,,input, output, columns] = process.argv
 
 assert(typeof input === 'string' && input.length > 0, new Error('input argument not provided'))
 assert(typeof output === 'string' && output.length > 0, new Error('output argument not provided'))
+assert(typeof columns === 'string' && columns.length > 0, new Error('columns argument not provided'))
 
-const columns = [
-  'Id',
-  'PostTypeId',
-  'AcceptedAnswerId',
-  'OwnerUserId',
-  'CreationDate',
-  'Title',
-  'Body',
-  'Tags',
-  'ParentId'
-];
+// TAGS Id,TagName,Count,ExcerptPostId,WikiPostId
+// POSTS Id,PostTypeId,AcceptedAnswerId,OwnerUserId,CreationDate,Title,Body,Tags,ParentId
 
 (async () => {
   console.log('before handleLines')
-  await handleLines(columns, input, output)
+  await handleLines(columns.split(','), input, output)
   console.log('after handleLines')
 })()
